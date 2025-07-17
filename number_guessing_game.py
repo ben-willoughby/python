aaaf69f6e742deddb984714632973042cd8e537c
Ben Willoughby <22011523+ben-willoughby@users.noreply.github.com>
2025-07-17 12:39:45 +0100

    initial commit

---
+++ number_guessing_game.py
@@ -0,0 +1,38 @@
+import art
+import random
+
+print(art.logo)
+print("Welcome to the Number Guessing Game!\n I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+lives = 0
+
+while difficulty != "easy" and difficulty != "hard":
+    print("Invalid difficulty")
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+if difficulty == "easy":
+    lives = 10
+elif difficulty == "hard":
+    lives = 5
+
+print(f"You have {lives} attempts remaining to guess the number.")
+guess = int(input("Make a guess: "))
+
+number = random.randint(1,100)
+
+while guess != number and lives > 0:
+    if guess > number:
+        print("Too high.")
+    elif guess < number:
+        print("Too low.")
+    print("Guess again.")
+    lives -= 1
+    print(f"You have {lives} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+
+if lives == 0 and guess != number:
+    print("You've run out of guesses. Refresh the page to run again.")
+elif lives > 0 and guess == number:
+    print(f"You got it! The answer was {number}.")
+
