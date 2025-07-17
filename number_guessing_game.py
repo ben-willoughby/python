1ddbbae97c679ce5a89f428d9079e79b2c0f2a48
Ben Willoughby <22011523+ben-willoughby@users.noreply.github.com>
2025-07-17 12:41:45 +0100

    fixed logo issue

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,7 +1,12 @@
-import art
+logo = r"""
+  / _ \_   _  ___  ___ ___  /__   \ |__   ___    /\ \ \_   _ _ __ ___ | |__   ___ _ __ 
+ / /_\/ | | |/ _ \/ __/ __|   / /\/ '_ \ / _ \  /  \/ / | | | '_ ` _ \| '_ \ / _ \ '__|
+/ /_\\| |_| |  __/\__ \__ \  / /  | | | |  __/ / /\  /| |_| | | | | | | |_) |  __/ |   
+\____/ \__,_|\___||___/___/  \/   |_| |_|\___| \_\ \/  \__,_|_| |_| |_|_.__/ \___|_| 
+"""
 import random
 
-print(art.logo)
+print(logo)
 print("Welcome to the Number Guessing Game!\n I'm thinking of a number between 1 and 100.")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
